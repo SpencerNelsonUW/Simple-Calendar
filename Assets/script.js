8821043762ff751c9e9c8f6afe5b8097ac3d3685
SpencerNelsonUW <spencernelsonn@gmail.com>
2022-08-02 13:25:16 -0700

    implemented the moment.js for the header to track date, also added some editable areas to write to-do tasks

---
+++ Assets/script.js
@@ -0,0 +1,6 @@
+//this variable is now equal to the default formatted moment function/api
+let now = moment();
+//this is where I tell the element to show its inner HTML as the variable created above
+document.getElementById("currentDay").innerHTML = now;
+
+
